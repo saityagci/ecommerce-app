544b94584875e7efc0331cc976306864c576b591
saityagci <saityagci@gmail.com>
2022-11-07 18:22:24 -0500

    many To many

---
+++ src/main/java/com/cydeo/entity/Category.java
@@ -0,0 +1,18 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Category extends BaseEntity {
+    private String name;
+
+
+}
---
+++ src/main/java/com/cydeo/entity/Product.java
@@ -0,0 +1,25 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Product extends BaseEntity{
+
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer remainingQuantity;
+
+    @ManyToMany
+    @JoinTable(name="product_category_rel",
+                joinColumns = @JoinColumn(name = "p_id"),
+                 inverseJoinColumns = @JoinColumn(name = "c_id"))
+    private List<Category> categoryList;
+}
